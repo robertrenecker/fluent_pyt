6e48892fc677bb5c903b58b507da42e38ab15da7
robert renecker <sylvan.rrenecker10@gmail.com>
2018-08-07 09:44:29 -0700

    initial

---
+++ main.py
@@ -0,0 +1,25 @@
+#test file
+import time;
+def main():
+    start_time = time.time();
+
+    
+
+
+
+
+
+
+
+
+
+
+    #Record total time
+    end_time = time.time();
+    total_time = (end_time-start_time);
+    print("Total time to execute: ", total_time);
+
+
+if __name__ == "__main__":
+    main();
+
